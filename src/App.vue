73eda76b191447ae068b29efd30415dfa2711516
molychn <46530729+molychn@users.noreply.github.com>
2023-09-09 22:05:11 +0800

    Vue3初始化3d项目结构

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import animate from './utils/index'
+
+animate()
+</script>
+
+<template></template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+canvas {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ src/utils/index.ts
@@ -0,0 +1,46 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+camera.position.z = 5
+camera.position.x = 5
+camera.position.y = 5
+camera.lookAt(new THREE.Vector3(0, 0, 0))
+
+// create AxesHelper
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
+
+// add OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.dampingFactor = 0.05
+
+animate()
+
+function animate() {
+  requestAnimationFrame(animate)
+
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+
+  renderer.render(scene, camera)
+}
+
+export default animate
